25b2b4a9b7c6aa5ae57ba236d2b5db3d7d073c8b
Jerry Zou <zhihui.zou@duke.edu>
2025-03-02 18:16:13 -0500

    march 2

--- PDFImageOCRPipeline/processOCRBetweenTXTandJSON.py
+++ PDFImageOCRPipeline/processOCRBetweenTXTandJSON.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 
 # This function turns an OCRed text stored in a JSON into a TXT file:
 
@@ -7,6 +7,7 @@ class processOCRTXTJSON:
         self.jsonFilePath = jsonFilePath
         self.txtFilePath = txtFilePath
     
+    #This turns one JSON into one combined TXT file:
     def jsonToTXT(self):
         with open(self.txtFilePath, "a") as txtFile:
 
@@ -15,19 +16,28 @@ class processOCRTXTJSON:
             for pageNum, pageContent in jsonOCRTextDict.items():
                 txtFile.write(pageContent)
 
+    #This turns one JSON into multiple TXT files, one for each key:value pair in JSON:
+    def jsonToMultiTXT(self, txtIndividualOutputFolder):
+        with open(self.jsonFilePath, "r") as jsonFile:
+            jsonOCRTextDict = json.load(jsonFile)
+        individualFileNameFormat = "image"
+        for imageNum, pageContent in jsonOCRTextDict.items():
+            txtOutputFileNameToCreate = individualFileNameFormat + str(imageNum) + ".txt"
+            txtOutputFilePathToCreate = os.path.join(txtIndividualOutputFolder, txtOutputFileNameToCreate)
+            with open(txtOutputFilePathToCreate, "w") as txtToWrite:
+                txtToWrite.write(pageContent)
+                print(f"Created file {txtOutputFileNameToCreate}.")
+        
+
     # We do not yet have a TXT to JSON tool, as when converting from JPG to OCR-ed texts, we automatically stored the content in JSON format.
     
 if __name__ == "__main__":
     # jsonFilePath = "C:/Users/zz341/Desktop/ECBC2024-5/OCRJSON/Vol3StoreOCRPerPage.json"
     # txtFilePath = "C:/Users/zz341/Desktop/ECBC2024-5/OCRTXT/v3FullOCR.txt"
 
-    jsonV4 = "C:/Users/zz341/Desktop/ECBC2024-5/OCRJSON/Vol4StoreOCRPerPage.json"
-    txtV4 = "C:/Users/zz341/Desktop/ECBC2024-5/OCRTXT/v4FullOCR.txt"
-
-    jsonV3 = "/Users/Jerry/Desktop/BassConnections2024-5/ECBC2024-5/OCRJSON/Vol3StoreOCRPerPage.json"
-    txtV3 = "/Users/Jerry/Desktop/BassConnections2024-5/ECBC2024-5/OCRTXT/v3FullOCR.txt"
-
-    jsonV2 = "/Users/Jerry/Desktop/BassConnections2024-5/ECBC2024-5/OCRJSON/Vol2StoreOCRPerPage.json"
-    txtV2 = "/Users/Jerry/Desktop/BassConnections2024-5/ECBC2024-5/OCRTXT/v2FullOCR.txt"
-    processOCRMachine = processOCRTXTJSON(jsonV2, txtV2)
-    processOCRMachine.jsonToTXT()
+    jsonFilePath = "/Users/Jerry/Desktop/BassConnections2024-5/ECBC2024-5/OCRJSON/OCR10imageTraining.json"
+    txtFilePath = ""
+    txtIndividualOutputFolder = "/Users/Jerry/Desktop/BassConnections2024-5/ECBC2024-5/OCRTXT/OCRTrainingIndividualTXT"
+    processOCRMachine = processOCRTXTJSON(jsonFilePath, txtFilePath)
+    # processOCRMachine.jsonToTXT()
+    processOCRMachine.jsonToMultiTXT(txtIndividualOutputFolder)
